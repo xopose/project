82b8b10dfafd7492c1457c1d9e342f356c6e721b
xopose <30976652+xopose@users.noreply.github.com>
2021-06-08 08:46:34 +0300

    Update X_Y_new.py

--- X_Y_new.py
+++ X_Y_new.py
@@ -5,12 +5,11 @@ from matplotlib.patches import Circle
 import tkinter as tk
 import random
 
-def E(q, r0, x, y):
-    """Return the electric field vector E=(Ex,Ey) due to charge q at r0."""
+def scale(q, r0, x, y):
     den = np.hypot(x-r0[0], y-r0[1])**3
     return q * (x - r0[0]) / den, q * (y - r0[1]) / den
 
-fields = 'X', 'Y', 'q_count', 'manual_x', 'manual_y', 'counter'
+fields = 'X', 'Y', 'q_count', 'manual_x1', 'manual_y1', 'counter_1', 'manual_x2', 'manual_y2', 'counter_2' 
 
 def makeform(root, fields):
   entries = []
@@ -36,59 +35,65 @@ def show(entries):
           N = number
         elif (field == "q_count"): # количество зарядов
           nq = number
-        elif (field == "manual_x"): # количество зарядов
-          manual_x = number
-        elif (field == "manual_y"): # количество зарядов
-          manual_y = number
-        elif (field == "counter"): # количество зарядов
-          counter = number
+        elif (field == "manual_x1"): # позиция x1
+          manual_x1 = number
+        elif (field == "manual_y1"): # позиция y1
+          manual_y1 = number
+        elif (field == "counter_1"): # знак заряда
+          counter_1 = number
+        elif (field == "manual_x2"): # позиция x2
+          manual_x2 = number
+        elif (field == "manual_y2"): # позиция y2
+          manual_y2 = number
+        elif (field == "counter_2"): # знак заряда
+          counter_2 = number
 
     
-    # Grid of x, y points
-    nx, ny = 16*M, 16*N
+    nx, ny = 10*M, 10*N
     x = np.linspace(0, M, nx)
     y = np.linspace(0, N, ny)
     X, Y = np.meshgrid(x, y)
+
     
-    # Create a multipole with nq charges of alternating sign, equally spaced
-    # on the unit circle.
     charges = []
     for num in range(nq):
         if (check.get() == 1):
               q = random.choice([-1, 1]) #выбор знака заряда
-              qx, qy = random.uniform(0, N), random.uniform(0, M) #выбор рандомной позиции
+              qx, qy = random.uniform(0, N-1), random.uniform(0, M-1) #выбор рандомной позиции
               charges.append((q, (qx, qy)))
         else:
-            q = counter
-            qx, qy = manual_x, manual_y #выбор ручной позиции
+            q = counter_1
+            qx, qy = manual_x1, manual_y1 #выбор ручной позиции 1
             charges.append((q, (qx, qy)))
-            check.set(1)
+            q = counter_2
+            qx, qy = manual_x2, manual_y2 #выбор ручной позиции 2
+            charges.append((q, (qx, qy)))
+            if (nq==2):
+                nq=0
+            else:
+                check.set(1)
 
 
-    # Electric field vector, E=(Ex, Ey), as separate components
     Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
     for charge in charges:
-        ex, ey = E(*charge, x=X, y=Y)
+        ex, ey = scale(*charge, x=X, y=Y)
         Ex += ex
         Ey += ey
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    # Plot the streamlines with an appropriate colormap and arrow style
+    
     color = 2 * np.log(np.hypot(Ex, Ey))
     ax.streamplot(x, y, Ex, Ey, color=color, linewidth=1, cmap=plt.cm.inferno,
                   density=2, arrowstyle='->', arrowsize=1.5)
 
-    # Add filled circles for the charges themselves
+    
     charge_colors = {True: '#aa0000', False: '#0000aa'}
     for q, pos in charges:
         ax.add_artist(Circle(pos, 0.05, color=charge_colors[q>0]))
 
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$y$')
-    ax.set_xlim(0,M)
-    ax.set_ylim(0,N)
+
     ax.set_aspect('equal')
     #plt.axis('off')
     plt.show()
